6136c5df9e11700374777b6b78cb24d16bf81ca7
alexliu95 <windylewisliu@hotmail.com>
2026-01-04 23:35:49 +0800

    update front page

--- src/app/[locale]/components/HeroNav.js
+++ src/app/[locale]/components/HeroNav.js
@@ -58,12 +58,12 @@ export default function HeroNav() {
                     </div>
                 </div>
             </div>
-            <div className="flex justify-center w-full py-3">
+            <div className="flex justify-center w-full py-3 items-center">
                 <div className="px-4 grid grid-cols-6 md:grid-cols-12">
                     <div className="col-span-5 md:col-span-10 lg:hidden hover:bg-white hover:fill-white active:bg-gray-400" onClick={()=>setIsMobileMenuShow(!isMobileMenuShow)}>
                         <svg width="30px" height="30px" fill="#000000" viewBox="0 0 24 24" id="menu-alt-3" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" className="icon flat-color hover:fill-white"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><rect id="primary" x="2" y="2" width="20" height="20" rx="2" style={{fill: "#000000"}}></rect><path id="secondary" d="M17,17H7a1,1,0,0,1,0-2H17a1,1,0,0,1,0,2Zm0-4H7a1,1,0,0,1,0-2H17a1,1,0,0,1,0,2Zm0-4H7A1,1,0,0,1,7,7H17a1,1,0,0,1,0,2Z" style={{fill: "#c4c4c4"}}></path></g></svg>
                     </div>
-                    <div className="hidden lg:inline-block lg:col-span-12 font-serif cursor-pointer">
+                    <div className="hidden lg:inline-block lg:col-span-12 lg:mt-40 font-serif cursor-pointer">
                         <ul className="flex gap-6 text-white">
                             <li><Link className="hover:text-white hover:bg-blue-400/60 px-3 py-5 transition select-none uppercase" href="/">{t('home')}</Link></li>
                             <li><Link className="hover:text-white hover:bg-blue-400/60 px-3 py-5 transition select-none uppercase" href="/pages/accomodations">{t('accomodations')}</Link></li>

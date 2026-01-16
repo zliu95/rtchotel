cb30bb4e7c2db0af7ad0ec1c4520d79376256bf6
alexliu95 <windylewisliu@hotmail.com>
2026-01-17 02:33:26 +0800

    playspeed to 0.2

--- src/app/[locale]/components/Hero.js
+++ src/app/[locale]/components/Hero.js
@@ -1,13 +1,23 @@
 'use client'
+import { useEffect, useRef } from "react";
 import HeroNav from "./HeroNav";
 
 export const Hero = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 0.2;
+    }
+  }, []);
+
   return (
     <section className="relative h-screen w-full overflow-hidden">
         <div className="absolute top-0 z-50 w-full transition-all duration-300 bg-card/95">
             <HeroNav />
         </div>
         <video
+          ref={videoRef}
           autoPlay
           muted
           loop
